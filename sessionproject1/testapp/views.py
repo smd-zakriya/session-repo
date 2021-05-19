f55fe5f88762e48c5b9368eacc1bf3aa980b6731
smd-zakriya <mohammadzakriya@gmail.com>
2021-05-19 21:19:35 +0530

    updated views

--- sessionproject1/testapp/views.py
+++ sessionproject1/testapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+#cookie_count
 def cookie_count_view(request):
     cookie_count=int(request.COOKIES.get('count',0))
     cookie_newcount=cookie_count+1
     response=render(request,'index.html',{'c_count':cookie_newcount})
     response.set_cookie('count',cookie_newcount)
     return response
-
+#session_count
 def session_count_view(request):
     session_count=request.session.get('count',0)
     session_newcount=session_count+1
